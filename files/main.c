127acfcd01acdf2770371af8bbb7d78cc220d37f
Manuel Rubio <manuel.rubio.arias@gmail.com>
2021-01-03 17:21:24 +0100

    v0301 -> map read

--- files/main.c
+++ files/main.c
@@ -6,7 +6,7 @@
 /*   By: mrubio <mrubio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 15:57:35 by mrubio            #+#    #+#             */
-/*   Updated: 2021/01/03 01:10:43 by mrubio           ###   ########.fr       */
+/*   Updated: 2021/01/03 17:20:47 by mrubio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,20 @@ int		main(int argc, char *argv[])
 	t_map	map;
 	t_vars	vars;
 
-	int x;
-	
-	x = 0;
 	fd = open("map.cub", O_RDONLY);
 	if (argc == 1)
 	{
 		map.map = (char **)malloc(100 * 100 * sizeof(char *));
-		while ((nbytes = ft_get_next_line(fd, &line)) > 0)
+		while ((nbytes = ft_get_next_line(fd, &line)) >= 0)
 		{
-			ft_printf("DOC: >%s\n", line);////////////////////////////////////////////////////
 			map = management_dotcub(line, map);
+			if (nbytes == 0)
+				break;
 		}
 		close(fd);
-		ft_printf("\nResolution: %dx%d\n", map.res_width, map.res_heigth);////////////////////
-		ft_printf("PATH NO: %s\n", map.path_N);///////////////////////////////////////////////
-		ft_printf("PATH SO: %s\n", map.path_S);///////////////////////////////////////////////
-		ft_printf("PATH WE: %s\n", map.path_W);///////////////////////////////////////////////
-		ft_printf("PATH EA: %s\n", map.path_E);///////////////////////////////////////////////
-		ft_printf("PATH SPRITE: %s\n", map.path_I);///////////////////////////////////////////
-		ft_printf("COLOR FLOOR: %x\n", map.color_floor);//////////////////////////////////////
-		ft_printf("COLOR CEILING: %x\n", map.color_ceiling);//////////////////////////////////
-		while (x <= 15)///////////////////////////////////////////////////////////////////////
-		{/////////////////////////////////////////////////////////////////////////////////////
-			ft_printf("MAP: %s\n", map.map[x]);///////////////////////////////////////////////
-			x++;//////////////////////////////////////////////////////////////////////////////
-		}/////////////////////////////////////////////////////////////////////////////////////
-		ft_printf("MAP: %s\n", map.map[x]);///////////////////////////////////////////////////
 		//<- FUNCION JUEGO
 	}
 	else
-	{
 		ft_printf("ERROR 01: MISSING MAP!");
-	}
 	return (0);
 }
